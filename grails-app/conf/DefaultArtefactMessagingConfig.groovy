5fcaabdbcd982f449d3254ab9a636d187414a2c6
Brian Saville <bsaville@adaptivecomputing.com>
2012-09-04 11:54:26 -0600

    Fix #1 and update docs

--- grails-app/conf/DefaultArtefactMessagingConfig.groovy
+++ grails-app/conf/DefaultArtefactMessagingConfig.groovy
@@ -1,10 +1,8 @@
-grails.plugins.artefactmessaging {
-	// Map of artefact type (serviceClasses) to grails-app folder name/plugin name
-	//	(can be null value if there is no associated artefact folder in grails-app or plugin)
-	artefacts = [
-		[
-			name:"service",
-			plugin:"services",
-		],
-	]
-}
+// Map of artefact type (serviceClasses) to grails-app folder name/plugin name
+//	(can be null value if there is no associated artefact folder in grails-app or plugin)
+artefactmessaging.artefacts = [
+	[
+		name:"service",
+		plugin:"services",
+	],
+]
