a4035cad1829295e09c3c46afb645573621f5559
Soeren Berg Glasius <soeren@glasius.dk>
2014-02-06 15:22:31 +0100

    Plugin as 1.0-SNAPSHOT

--- ArtefactMessagingGrailsPlugin.groovy
+++ ArtefactMessagingGrailsPlugin.groovy
@@ -6,7 +6,7 @@ class ArtefactMessagingGrailsPlugin {
 	static Logger log = Logger.getLogger('grails.app.conf.BootStrap')
 
     // the plugin version
-    def version = "0.9"
+    def version = "1.0-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
